58a96890a6264d528aafde95b2b031650b3adeb9
Richard Sirokman <rich@desertwave.cloud>
2019-08-21 10:37:22 -0600

    fixed file handling for GET and POST

--- trackstuffapi.py
+++ trackstuffapi.py
@@ -1,15 +1,16 @@
-import simplejson as json
+import json
+import os
 from flask import Flask, request, jsonify
 from flask_restful import Api, reqparse, Resource
 
 app = Flask(__name__)
-containers = []
 
 def importDatabase():
-    global containers
+    #data = {}
     try:
-        with open("apidata.txt") as file:
-            if file.read(1):
+        with open("apidata.txt", "r") as file:
+            #data = json.load(file)
+            if os.stat("apidata.txt").st_size > 0:
                 data = json.load(file)
             else:
                 data = None
@@ -34,28 +35,50 @@ def allstuff():
 
 @app.route('/locations', methods=['GET','POST'])
 def get_all_locations():
-    locStuff = importDatabase()
-    if locStuff == None:
-        return "You don't have any locations.", 404
+  #  locStuff= {
+   #     "desk":"paper",
+    #    "chair":"pad",
+     #   "splashscreen":"false"
+    #} 
+    #locStuff= importDatabase()
+   # locStr= json.dumps(locStuff)
+   # print("This is locStr: " +locStr)
+    #locStuff= importDatabase()
+    #with open('apidata.txt', 'w') as outfile:
+     #       json.dump(locStuff, outfile)
+    
     if request.method == 'GET':
-        outputLocations = []
+        #outputLocations = {}
 
-        for i in range(0,len(locStuff)):
-            outputLocations.append(locStuff[i])
+        #for i in range(0,len(locStuff)):
+         #   outputLocations.append(locStuff[i])
         
-        output = str(outputLocations)
-        print(str(output))
-        return output
+        #output = str(outputLocations)
+        #with open('apidata.txt', 'r') as file:
+        #    locDict=json.load(file)
+        locDict=importDatabase()
+        if locDict == None:
+            return "You don't have any locations.", 404
+        locStr = jsonify(locDict)
+        print("This is locStr: "+ str(locStr))
+        return (locStr),200
 
     if request.method == 'POST':
-        addLocation = []
-        stringLoc=(request.get_data().decode('utf8'))
-        addLocation.append(stringLoc)
-        locStuff.append(addLocation)
+        addLocation = {
+            "theme": "bluespring",
+            "size": "small",
+            "splashscreen": "false"
+        }
+        #if locStuff == None:
+         #   locStuff = {}
+        #stringLoc=(request.get_data().decode('utf8'))
+        #addLocation.update(stringLoc)
+        #locStuff.append(addLocation)
         message = "Successfully added location "
-        with open('data.txt', 'w') as outfile:
-            json.dump(locStuff, outfile)
-        return (message + stringLoc),201
+        with open('apidata.txt', 'w') as outfile:
+            json.dump(addLocation, outfile)
+        #return (message + stringLoc),201
+        return (message),201
 
 @app.route('/locations/<loc>', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def specific_locations():
